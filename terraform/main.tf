9a35358306f0b0e57c09d6bef64bee7f1f096f51
Dmitrii-I <izgurskii@gmail.com>
2019-09-27 22:23:54 +0200

    Add bucket for logs and put bucket names into vars

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,10 @@
+locals {
+  site_names = {
+    "prod": "bash-template.com",
+    "dev": "bash-template.com-dev"
+  }
+}
+
 terraform {
   backend "local" {
     path = "~/bash-template/terraform/terraform.tfstate"
@@ -9,13 +16,25 @@ provider "aws" {
   profile   = "dmitrii-bash-template.com"
 }
 
-resource "aws_s3_bucket" "b" {
-  bucket = "bash-template.com-${terraform.workspace}"
+resource "aws_s3_bucket" "logs" {
+  bucket = "${local.site_names[terraform.workspace]}-site-logs"
+  acl = "log-delivery-write"
+}
+
+resource "aws_s3_bucket" "site" {
+
+  bucket = local.site_names[terraform.workspace]
   acl    = "public-read"
 
   website {
     index_document = "index.html"
     error_document = "error.html"
-
   }
+
+  logging {
+    target_bucket = aws_s3_bucket.logs.id
+    target_prefix = ""
+  }
+
 }
+
