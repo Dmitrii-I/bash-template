f698e23fec354d790a69cf7f1b09b96e6d52cc37
Dmitrii-I <izgurskii@gmail.com>
2020-04-02 22:34:01 +0200

    Adjust bucket name, delete record
    Adjust bucket name to be same as domain name, else it won't work. Get
    rid of www CNAME as it does not seem to work.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,6 @@
+# https://gist.github.com/nagelflorian/67060ffaf0e8c6016fa1050b6a4e767a
+# https://westerndevs.com/2018-10-12-cloudfront-terraform/
+
 terraform {
   backend "remote" {
     hostname      = "app.terraform.io"
@@ -12,7 +15,7 @@ terraform {
 locals {
   domain_name         = "bash-template.com"
   aws_default_region  = "eu-central-1"
-  website_bucket      = "bash-template-site"
+  website_bucket      = "bash-template.com"
   website_logs_bucket = "bash-template-site-logs"
 }
 
@@ -60,10 +63,13 @@ resource "aws_s3_bucket" "site" {
 }
 
 resource "aws_s3_bucket_object" "template" {
-  bucket  = local.website_bucket
-  key     = "template.sh"
-  source  = "../template.sh"
-  etag    = filemd5("../template.sh")
+  bucket        = local.website_bucket
+  key           = "template.sh"
+  source        = "../template.sh"
+  etag          = filemd5("../template.sh")
+
+  # Ensure that Firefox opens it as page and does not download the file.
+  content_type  = "text/plain"
 }
 
 resource "aws_route53_zone" "bash_template_com" {
@@ -75,18 +81,7 @@ resource "aws_route53_record" "apex" {
   name    = local.domain_name
   type    = "A"
   alias {
-    name                    = aws_s3_bucket.site.website_endpoint
-    zone_id                 = aws_s3_bucket.site.hosted_zone_id
-    evaluate_target_health  = false
-  }
-}
-
-resource "aws_route53_record" "www" {
-  zone_id = aws_route53_zone.bash_template_com.zone_id
-  name    = "www.${local.domain_name}"
-  type    = "A"
-  alias {
-    name                    = aws_s3_bucket.site.website_endpoint
+    name                    = aws_s3_bucket.site.website_domain
     zone_id                 = aws_s3_bucket.site.hosted_zone_id
     evaluate_target_health  = false
   }
