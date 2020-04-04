1f98050bd544c41117e2214b295bb9db48823e59
Dmitrii-I <izgurskii@gmail.com>
2020-04-04 20:56:06 +0200

    Add cloudformation domain name to outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,7 @@ output "website_endpoint" {
 output "name_servers" {
   value = aws_route53_zone.bash_template_com.name_servers
 }
+
+output cloudfront_domain_name {
+  value = aws_cloudfront_distribution.bash-template.domain_name
+}
