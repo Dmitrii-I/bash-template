4a4d0e264e5a3cb3e51c0e3cabe8447ba792b49d
Dmitrii-I <izgurskii@gmail.com>
2020-04-09 21:23:08 +0200

    Add script to print CloudFront stats

---
+++ cloudfront-stats.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -o nounset      # exit with non-zero status if expansion is attempted on an unset variable
+set -o errexit      # exit immediatelly if a pipeline, a list, or a compound command fails
+set -o pipefail     # failures in pipe in the commands before last one, also count as failures
+
+# Trapping non-zero exit codes:
+on_error() {
+    line_num="$1"
+    echo "Caught error on line $line_num"
+}
+
+on_exit() {
+    true
+}
+
+on_interrupt() {
+    true
+}
+trap 'on_error $LINENO' ERR
+trap on_exit EXIT
+trap on_interrupt INT
+
+cloudfront_distribution_id="$(cd ~/bash-template/terraform; terraform output cloudfront_distribution_id)"
+
+aws cloudwatch get-metric-statistics \
+    --region us-east-1 \
+    --profile 8f302fabec669d3401657e9e71b29b46 \
+    --start-time "$(date --utc --iso-8601 -d '-14 days')T00:00:00Z" \
+    --end-time "$(date --utc --iso-8601 -d '+1 day')T00:00:00Z" \
+    --namespace "AWS/CloudFront" \
+    --statistics Sum \
+    --period 3600 \
+    --metric-name Requests \
+    --dimensions Name=DistributionId,Value="$cloudfront_distribution_id" Name=Region,Value=Global \
+    | jq '.Datapoints[] | [.Timestamp, .Sum] | @tsv' --raw-output \
+    | sort
+
