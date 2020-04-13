92ec61a3116460bbea6ce717be34cecf39b86c15
Dmitrii-I <izgurskii@gmail.com>
2020-04-13 13:08:03 +0200

    Update template.sh

--- template.sh
+++ template.sh
@@ -1,22 +1,3 @@
 #!/usr/bin/env bash
-# bash-template.com, version 1
 
-set -o nounset
-set -o errexit
-set -o pipefail
-
-on_error() {
-    echo "Caught error on line $1"
-}
-
-on_exit() {
-    true
-}
-
-on_interrupt() {
-    true
-}
-trap 'on_error $LINENO' ERR
-trap on_exit EXIT
-trap on_interrupt INT
-
+POSIXLY_CORRECT=1 set -o errexit && set -o nounset && set -o pipefail && unset POSIXLY_CORRECT
