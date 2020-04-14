2f9c0592fd9a1b95dd1a283baf45243699657df6
Dmitrii-I <izgurskii@gmail.com>
2020-04-14 23:29:21 +0200

    Add echo statement for debugging
    GitHub Actions is still passing. I suspect that shellcheck actually does
    not test any file when the test script is run on GitHub.

--- tests/test-shell-scripts-with-shellcheck.sh
+++ tests/test-shell-scripts-with-shellcheck.sh
@@ -5,6 +5,8 @@ POSIXLY_CORRECT=1 set -o errexit && set -o nounset && set -o pipefail && unset P
 echo Test shell scripts with shellcheck
 files="$(find ~/bash-template -iname '*.sh')"
 for f in $files; do
+    echo "Test $f with shellcheck"
     shellcheck "$f"
+    echo PASSED
 done
 echo PASSED
