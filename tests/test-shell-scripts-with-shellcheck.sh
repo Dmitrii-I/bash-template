7ce893e7234e74352eb18ab79c90711595bc1bb6
Dmitrii-I <izgurskii@gmail.com>
2020-04-14 23:44:42 +0200

    Fix find command, remove debug statements
    Fix find command by using `-L` option to follow symlinks. The directory
    ~/bash-template inside the GitHub Action runner is actually a symlink
    and that is why `find` was not finding anything.

--- tests/test-shell-scripts-with-shellcheck.sh
+++ tests/test-shell-scripts-with-shellcheck.sh
@@ -3,9 +3,7 @@
 POSIXLY_CORRECT=1 set -o errexit && set -o nounset && set -o pipefail && unset POSIXLY_CORRECT
 
 echo Test shell scripts with shellcheck
-files="$(find ~/bash-template -iname '*.sh')"
-find --version
-which find
+files="$(find -L ~/bash-template -iname '*.sh')"
 for f in $files; do
     echo "Test $f with shellcheck"
     shellcheck "$f"
